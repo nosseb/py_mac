84f8e8e555ffee34b41af1010f16fb889e60264d
Robinson Besson <robinson.besson@nosseb.fr>
2025-01-27 10:10:33 +0100

    exception handling when within locks

--- TODOs.md
+++ TODOs.md
@@ -1,6 +1,9 @@
 
-- [ ] Better handle exceptions when using locks
+
 - [ ] Implement re-attempts when we are not the destination of the received message
 - [ ] Provide missing register description
 - [ ] Implement ROS2 subscriber
+
+
+- [x] Better handle exceptions when using locks
 - [x] Convert `operating_modes` to an enum
--- py_mac/MAC50Motor.py
+++ py_mac/MAC50Motor.py
@@ -61,34 +61,31 @@ class MAC50Motor:
         self.status_lock = Lock()
 
         # Update the object tp match the motor
-        self.status_lock.acquire()
-        self.status = {
-            "operating mode": "", # Cant be set here since get_mode_* requires self.status to exist
-            "max velocity": int.from_bytes(self.read_register("V_SOLL"), byteorder="little"),
-            "max acceleration": int.from_bytes(self.read_register("A_SOLL"), byteorder="little"),
-            "max_torque": int.from_bytes(self.read_register("T_SOLL"), byteorder="little"),
-            "gear ratio nomitor": int.from_bytes(self.read_register("GEARF1"), byteorder="little"),
-            "gear ratio denominator": int.from_bytes(self.read_register("GEARF2"), byteorder="little"),
-            "max winding energy": int.from_bytes(self.read_register("I2TLIM"), byteorder="little"),
-            "max dumped energy": int.from_bytes(self.read_register("UITLIM"), byteorder="little"),
-            "max position error": int.from_bytes(self.read_register("FLWERRMAX"), byteorder="little"),
-            "max velocity error": int.from_bytes(self.read_register("FNCERRMAX"), byteorder="little"),
-            "min position": int.from_bytes(self.read_register("MIN_P_IST"), byteorder="little", signed=True),
-            "max position": int.from_bytes(self.read_register("MAX_P_IST"), byteorder="little", signed=True),
-            "emergency deceleration": int.from_bytes(self.read_register("ACC_EMERG"), byteorder="little"),
-            "starting mode": int.from_bytes(self.read_register("STARTMODE"), byteorder="little"),
-            "home position": int.from_bytes(self.read_register("P_HOME"), byteorder="little", signed=True),
-            "homing velocity": int.from_bytes(self.read_register("V_HOME"), byteorder="little"),
-            "homing mode": int.from_bytes(self.read_register("HOME_MODE"), byteorder="little"),
-            "min supply voltage": int.from_bytes(self.read_register("MIN_U_SUP"), byteorder="little"),
-            "motor type": int.from_bytes(self.read_register("MOTORTYPE"), byteorder="little"),
-            "serial number": int.from_bytes(self.read_register("SERIAL"), byteorder="little"),
-            "address": int.from_bytes(self.read_register("MYADDR"), byteorder="little"),
-            "hardware version": int.from_bytes(self.read_register("HWVERSION"), byteorder="little"),
-        }
-        self.status_lock.release()
-
-        self.get_mode_id()
+        with self.status_lock:
+            self.status = {
+                "operating mode": OperatingMode(int.from_bytes(self.read_register("MODE_REG"), byteorder="little")),
+                "max velocity": int.from_bytes(self.read_register("V_SOLL"), byteorder="little"),
+                "max acceleration": int.from_bytes(self.read_register("A_SOLL"), byteorder="little"),
+                "max_torque": int.from_bytes(self.read_register("T_SOLL"), byteorder="little"),
+                "gear ratio nomitor": int.from_bytes(self.read_register("GEARF1"), byteorder="little"),
+                "gear ratio denominator": int.from_bytes(self.read_register("GEARF2"), byteorder="little"),
+                "max winding energy": int.from_bytes(self.read_register("I2TLIM"), byteorder="little"),
+                "max dumped energy": int.from_bytes(self.read_register("UITLIM"), byteorder="little"),
+                "max position error": int.from_bytes(self.read_register("FLWERRMAX"), byteorder="little"),
+                "max velocity error": int.from_bytes(self.read_register("FNCERRMAX"), byteorder="little"),
+                "min position": int.from_bytes(self.read_register("MIN_P_IST"), byteorder="little", signed=True),
+                "max position": int.from_bytes(self.read_register("MAX_P_IST"), byteorder="little", signed=True),
+                "emergency deceleration": int.from_bytes(self.read_register("ACC_EMERG"), byteorder="little"),
+                "starting mode": OperatingMode(int.from_bytes(self.read_register("STARTMODE"), byteorder="little")),
+                "home position": int.from_bytes(self.read_register("P_HOME"), byteorder="little", signed=True),
+                "homing velocity": int.from_bytes(self.read_register("V_HOME"), byteorder="little"),
+                "homing mode": int.from_bytes(self.read_register("HOME_MODE"), byteorder="little"),
+                "min supply voltage": int.from_bytes(self.read_register("MIN_U_SUP"), byteorder="little"),
+                "motor type": int.from_bytes(self.read_register("MOTORTYPE"), byteorder="little"),
+                "serial number": int.from_bytes(self.read_register("SERIAL"), byteorder="little"),
+                "address": int.from_bytes(self.read_register("MYADDR"), byteorder="little"),
+                "hardware version": int.from_bytes(self.read_register("HWVERSION"), byteorder="little"),
+            }
 
     def __del__(self):
         self.serial.close()
@@ -105,10 +102,9 @@ class MAC50Motor:
 
         message = [0x50, 0x50, 0x50, self.address, 0xff ^ self.address, reg_num, 0xff ^ reg_num, 0xaa, 0xaa]
 
-        self.serial_lock.acquire()
-        self.serial.write(bytes(message))
-        response = self.serial.read(19)
-        self.serial_lock.release()
+        with self.serial_lock:
+            self.serial.write(bytes(message))
+            response = self.serial.read(19)
 
         expected      = [0x52, 0x52, 0x52, 0x00, 0xff, reg_num, 0xff ^ reg_num, 0x04, 0xfb,  0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xaa, 0xaa]
         frame_mask    = [0xff, 0xff, 0xff, 0x00, 0x00, 0x00, 0x00, 0xff, 0xff, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xff, 0xff]
@@ -150,10 +146,9 @@ class MAC50Motor:
         data_with_complement = [b for pair in zip(data, complement) for b in pair]
         message = [0x52, 0x52, 0x52, self.address, 0xff ^ self.address, reg_num, 0xff ^ reg_num, len(data), 0xff ^ len(data)] + data_with_complement + [0xaa, 0xaa]
 
-        self.serial_lock.acquire()
-        self.serial.write(bytes(message))
-        response = self.serial.read(3)
-        self.serial_lock.release()
+        with self.serial_lock:
+            self.serial.write(bytes(message))
+            response = self.serial.read(3)
 
         expected = [0x11, 0x11, 0x11]
         if response != expected:
@@ -200,14 +195,11 @@ class MAC50Motor:
         Get the operating mode of the motor.
         :return: current operating mode
         """
-        self.status_lock.acquire()
+        with self.status_lock:
+            mode = OperatingMode(int.from_bytes(self.read_register("MODE_REG"), byteorder="little"))
 
-        mode = OperatingMode(int.from_bytes(self.read_register("MODE_REG"), byteorder="little"))
-
-        # update the object to match the motor
-        self.status["operating mode"] = mode
-
-        self.status_lock.release()
+            # update the object to match the motor
+            self.status["operating mode"] = mode
 
         return mode
 
@@ -225,14 +217,11 @@ class MAC50Motor:
         if not isinstance(mode, OperatingMode):
             raise ValueError("Invalid mode")
 
-        self.status_lock.acquire()
+        with self.status_lock:
+            self.write_register("MODE_REG", mode.value)
 
-        self.write_register("MODE_REG", mode.value)
-
-        # update the object to match the motor
-        self.status["operating mode"] = mode
-
-        self.status_lock.release()
+            # update the object to match the motor
+            self.status["operating mode"] = mode
 
     def get_position(self)->int:
         """
@@ -247,12 +236,9 @@ class MAC50Motor:
         :param position: target position
         :param ignore_mode: if True, the function will not check if the motor is in position mode
         """
-        self.status_lock.acquire()
+        with self.status_lock:
+            # check that the motor is in position mode
+            if not ignore_mode and self.status["operating mode"] != OperatingMode.POSITION:
+                raise ValueError("Motor must be in position mode")
 
-        # check that the motor is in position mode
-        if not ignore_mode and self.status["operating mode"] != OperatingMode.POSITION:
-            raise ValueError("Motor must be in position mode")
-
-        self.write_register("P_SOLL", position)
-
-        self.status_lock.release()
+            self.write_register("P_SOLL", position)
