bd08567d47db598e347d7c8ee725094fe5760b72
Robinson Besson <robinson.besson@nosseb.fr>
2025-01-27 10:24:26 +0100

    check limits before switching to position mode

--- TODOs.md
+++ TODOs.md
@@ -7,3 +7,4 @@
 
 - [x] Better handle exceptions when using locks
 - [x] Convert `operating_modes` to an enum
+- [x] Ensure the motor is within the limits before switching to position mode
--- py_mac/MAC50Motor.py
+++ py_mac/MAC50Motor.py
@@ -61,31 +61,8 @@ class MAC50Motor:
         self.status_lock = Lock()
 
         # Update the object tp match the motor
-        with self.status_lock:
-            self.status = {
-                "operating mode": OperatingMode(int.from_bytes(self.read_register("MODE_REG"), byteorder="little")),
-                "max velocity": int.from_bytes(self.read_register("V_SOLL"), byteorder="little"),
-                "max acceleration": int.from_bytes(self.read_register("A_SOLL"), byteorder="little"),
-                "max_torque": int.from_bytes(self.read_register("T_SOLL"), byteorder="little"),
-                "gear ratio nomitor": int.from_bytes(self.read_register("GEARF1"), byteorder="little"),
-                "gear ratio denominator": int.from_bytes(self.read_register("GEARF2"), byteorder="little"),
-                "max winding energy": int.from_bytes(self.read_register("I2TLIM"), byteorder="little"),
-                "max dumped energy": int.from_bytes(self.read_register("UITLIM"), byteorder="little"),
-                "max position error": int.from_bytes(self.read_register("FLWERRMAX"), byteorder="little"),
-                "max velocity error": int.from_bytes(self.read_register("FNCERRMAX"), byteorder="little"),
-                "min position": int.from_bytes(self.read_register("MIN_P_IST"), byteorder="little", signed=True),
-                "max position": int.from_bytes(self.read_register("MAX_P_IST"), byteorder="little", signed=True),
-                "emergency deceleration": int.from_bytes(self.read_register("ACC_EMERG"), byteorder="little"),
-                "starting mode": OperatingMode(int.from_bytes(self.read_register("STARTMODE"), byteorder="little")),
-                "home position": int.from_bytes(self.read_register("P_HOME"), byteorder="little", signed=True),
-                "homing velocity": int.from_bytes(self.read_register("V_HOME"), byteorder="little"),
-                "homing mode": int.from_bytes(self.read_register("HOME_MODE"), byteorder="little"),
-                "min supply voltage": int.from_bytes(self.read_register("MIN_U_SUP"), byteorder="little"),
-                "motor type": int.from_bytes(self.read_register("MOTORTYPE"), byteorder="little"),
-                "serial number": int.from_bytes(self.read_register("SERIAL"), byteorder="little"),
-                "address": int.from_bytes(self.read_register("MYADDR"), byteorder="little"),
-                "hardware version": int.from_bytes(self.read_register("HWVERSION"), byteorder="little"),
-            }
+        self.status = {}
+        self.refresh_satus()
 
     def __del__(self):
         self.serial.close()
@@ -217,6 +194,14 @@ class MAC50Motor:
         if not isinstance(mode, OperatingMode):
             raise ValueError("Invalid mode")
 
+        if mode == self.status["operating mode"]:
+            return
+
+        if mode == OperatingMode.POSITION and (self.status["min position"] != 0 or self.status["max position"] != 0):
+            position = self.get_position()
+            if position < self.status["min position"] or position > self.status["max position"]:
+                raise ValueError("Position out of bounds")
+
         with self.status_lock:
             self.write_register("MODE_REG", mode.value)
 
@@ -242,3 +227,33 @@ class MAC50Motor:
                 raise ValueError("Motor must be in position mode")
 
             self.write_register("P_SOLL", position)
+
+    def refresh_satus(self)->None:
+        """"
+        Refresh the status of the motor.
+        """
+        with self.status_lock:
+            self.status = {
+                "operating mode": OperatingMode(int.from_bytes(self.read_register("MODE_REG"), byteorder="little")),
+                "max velocity": int.from_bytes(self.read_register("V_SOLL"), byteorder="little"),
+                "max acceleration": int.from_bytes(self.read_register("A_SOLL"), byteorder="little"),
+                "max_torque": int.from_bytes(self.read_register("T_SOLL"), byteorder="little"),
+                "gear ratio nomitor": int.from_bytes(self.read_register("GEARF1"), byteorder="little"),
+                "gear ratio denominator": int.from_bytes(self.read_register("GEARF2"), byteorder="little"),
+                "max winding energy": int.from_bytes(self.read_register("I2TLIM"), byteorder="little"),
+                "max dumped energy": int.from_bytes(self.read_register("UITLIM"), byteorder="little"),
+                "max position error": int.from_bytes(self.read_register("FLWERRMAX"), byteorder="little"),
+                "max velocity error": int.from_bytes(self.read_register("FNCERRMAX"), byteorder="little"),
+                "min position": int.from_bytes(self.read_register("MIN_P_IST"), byteorder="little", signed=True),
+                "max position": int.from_bytes(self.read_register("MAX_P_IST"), byteorder="little", signed=True),
+                "emergency deceleration": int.from_bytes(self.read_register("ACC_EMERG"), byteorder="little"),
+                "starting mode": OperatingMode(int.from_bytes(self.read_register("STARTMODE"), byteorder="little")),
+                "home position": int.from_bytes(self.read_register("P_HOME"), byteorder="little", signed=True),
+                "homing velocity": int.from_bytes(self.read_register("V_HOME"), byteorder="little"),
+                "homing mode": int.from_bytes(self.read_register("HOME_MODE"), byteorder="little"),
+                "min supply voltage": int.from_bytes(self.read_register("MIN_U_SUP"), byteorder="little"),
+                "motor type": int.from_bytes(self.read_register("MOTORTYPE"), byteorder="little"),
+                "serial number": int.from_bytes(self.read_register("SERIAL"), byteorder="little"),
+                "address": int.from_bytes(self.read_register("MYADDR"), byteorder="little"),
+                "hardware version": int.from_bytes(self.read_register("HWVERSION"), byteorder="little"),
+            }
